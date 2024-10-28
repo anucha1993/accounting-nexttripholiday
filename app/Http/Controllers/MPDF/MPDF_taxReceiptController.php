7e737b594f26d8b2b5a5457ff656d3c686bf1078
anucha1993 <108377127+anucha1993@users.noreply.github.com>
2024-10-28 17:05:21 +0700

    updated

--- app/Http/Controllers/MPDF/MPDF_taxReceiptController.php
+++ app/Http/Controllers/MPDF/MPDF_taxReceiptController.php
@@ -115,8 +115,8 @@ class MPDF_taxReceiptController extends Controller
        ->where('vat_status', 'vat')
        ->sum('product_sum');
        $texreceipt = taxinvoiceModel::where('invoice_number',$invoiceModel->invoice_number)->first();
-       $paymentDeposit = paymentModel::where('payment_doc_number',$invoiceModel->quote_number)->sum('payment_total');
-       $payment = paymentModel::where('payment_doc_number',$invoiceModel->invoice_quote)
+       $paymentDeposit = paymentModel::where('payment_quote_id',$quotationModel->quote_id)->sum('payment_total');
+       $payment = paymentModel::where('payment_quote_id',$quotationModel->quote_id)
        ->leftjoin('bank','bank.bank_id','payments.payment_bank_number')
        ->latest('payments.payment_date_time')->first();
        // ดึง HTML จาก Blade Template
