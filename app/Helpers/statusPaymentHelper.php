b2aedea55e8947c6319edca47b6feba4eda2ba4e
anucha1993 <108377127+anucha1993@users.noreply.github.com>
2025-03-19 16:16:53 +0700

    sdsadasd

--- app/Helpers/statusPaymentHelper.php
+++ app/Helpers/statusPaymentHelper.php
@@ -37,37 +37,3 @@ if (!function_exists('getQuoteStatusPayment')) {
     }
 }
 
-if (!function_exists('getQuoteStatusPaymentReport')) {
-    function getQuoteStatusPaymentReport($quotationModel)
-    {
-        $now = Carbon::now();
-        $status = '';
-        // ตรวจสอบ payment_status ผ่านความสัมพันธ์ quotePayment
-        if ($quotationModel->quotePayment && $quotationModel->quotePayment->payment_status === 'refund') {
-
-            $status = 'รอคืนเงิน';
-        } elseif ($quotationModel->quote_status === 'cancel') {
-            $status = 'ยกเลิกการสั่งซื้อ';
-    
-        } elseif ($quotationModel->quote_status === 'success') {
-            $status = 'ชำระเงินครบแล้ว';
-        } elseif ($quotationModel->payment > 0) {
-            $status = 'รอชำระเงินเต็มจำนวน';
-        } elseif ($quotationModel->quote_payment_type === 'deposit') {
-            if ($now->gt(Carbon::parse($quotationModel->quote_payment_date))) {
-                $status = 'เกินกำหนดชำระเงิน';
-            } else {
-                $status = 'รอชำระเงินมัดจำ';
-            }
-        } elseif ($quotationModel->quote_payment_type === 'full') {
-            if ($now->gt(Carbon::parse($quotationModel->quote_payment_date_full))) {
-                $status = 'เกินกำหนดชำระเงิน';
-            } else {
-                $status = 'รอชำระเงินเต็มจำนวน';
-            }
-        } else {
-            $status = 'รอชำระเงิน';
-        }
-        return $status;
-    }
-}
